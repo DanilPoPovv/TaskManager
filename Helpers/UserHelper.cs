7504c2a8881f7bb1922bb6e52c84ff92159128ad
Danil <mastermcpe691@gmail.com>
2026-03-08 12:16:05 +0600

    Refactoring TaskAdd Service

---
+++ Helpers/UserHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Helpers
+{
+    public class UserHelper 
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserHelper(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<User> GetByIdOrThrow(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                throw new Exception("User not found");
+            return user;
+        }
+        public async Task<User> GetByNameOrThrow(string userName)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+            if (user == null)
+                throw new Exception("User not found");
+            return user;
+        }
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.AddJwtAuthenticationSchemes();
-
+Expression expression = new Expressin();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -31,13 +31,12 @@ builder.Services.AddControllers(options =>
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+///app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseRouting();
